eb64938dd4326d3f581f1c17dcf5ee4697eff819
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-04-19 10:19:33 +0100

    Add dark theme to the website.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        
+    }
+}
 
+
+
+function toggleTheme() {
+    var theme = document.getElementById("theme");
+    if (theme.getAttribute('href') == 'css/main.css') {
+        theme.setAttribute('href', 'css/dark.css');
+    } else {
+        theme.setAttribute('href', 'css/main.css');
+    }
+}
